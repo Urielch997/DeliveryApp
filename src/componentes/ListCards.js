622d0932c959905243d694cc649ffd5275ab47b5
Kevin Hernandez <kevingeogarcia@gmail.com>
2020-07-22 23:44:29 -0600

    add filter ListCards, add hook for filter

--- src/componentes/ListCards.js
+++ src/componentes/ListCards.js
@@ -1,79 +1,118 @@
-import React, { Component } from 'react';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import {Link} from 'react-router-dom';
-import db from '../fireConfig';
-
-class ListCards extends Component{
-
-    state = {
-        img:[],
-        items:[],
-        loading:false,
+import React, { useState, useEffect } from "react";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import SearchIcon from "@material-ui/icons/Search";
+import { Link } from "react-router-dom";
+import db from "../fireConfig";
+import useSearch from "../hooks/useSearch";
+const ListCards = () => {
+  const [filteredData, setSearch, setSourceData] = useSearch();
+  const [img, setImg] = useState([]);
+  const [items, setItems] = useState([]);
+  const [loading, setLoadig] = useState();
+  const addData = () => {
+    setSourceData(img);
+  };
+  const getData = () => {
+    db.collection("imagenes")
+      .get()
+      .then((snapshot) => {
+        setImg(
+          snapshot.docs.map((data) => {
+            return {
+              id: data.id,
+              nombre: data.data().nombre,
+              url: data.data().url,
+              restaurante: data.data().restaurante,
+              horario: data.data().horario,
+            };
+          })
+        );
+      })
+      .then(() => {
+        setLoadig(true);
+      });
+  };
+  const getHoras = (hora) => {
+    var h;
+    if (hora >= 12) {
+      h = hora - 12;
     }
-    
-    componentDidMount(){
-        this.getData();
+    if (parseInt(hora) === 0) {
+      h = 12;
     }
+    return h;
+  };
+  useEffect(() => {
+    getData();
+  }, []);
 
-    getData() {
-        db.collection('imagenes').get().then(snapshot =>{
-            this.setState({
-                img:snapshot.docs.map(data =>{
-                    return{id:data.id,nombre:data.data().nombre,url:data.data().url,restaurante:data.data().restaurante,horario:data.data().horario}
-                })
-            })
-        }).then(()=>{
-            this.setState({
-                loading:true
-            })
-        })
-    }
+  useEffect(() => {
+    addData();
+  }, [img]);
 
-    getHoras(hora){
-        var h;
-        if(hora>=12){
-           h  = hora-12;
-        }
-        if(parseInt(hora) === 0){
-            h = 12;
-        }
-        return(h)
-    }
+  useEffect(() => {
+    addData();
+    console.log(filteredData);
+  }, [filteredData]);
 
-
-    listtarget = () => (
-        this.state.img.map((datos) =>{
-            const hora = (new Date()).getHours();
-            const apertura = datos.horario[0].substr(0,2);
-            const cierre = datos.horario[1].substr(0,2);
-            return(
-            <div className="cards-list" key={datos.id}>
-            <Link to={{     
-         pathname: '/detalles',
-         state:datos
-        }}>
-                <div className="card-img">
-                {parseInt(hora) >= apertura && parseInt(hora) < cierre ? '' : <div className='close'><label>CERRADO</label></div>}
-                        <img src={datos.url} alt='Imagen' className='card-image'/>
-                    
-                </div>
-    <div className="text-card-footer"><span>{datos.restaurante}</span><span className="active">{parseInt(hora) >= apertura && parseInt(hora) <= cierre? 'Abierto' : 'Cerrado'}</span></div>
-            </Link>
+  return (
+    <div className="center">
+      <div className="cards-container">
+        <input
+          type="text"
+          id="search"
+          onChange={(e) => setSearch(e.target.value)}
+          className="search-input"
+          placeholder="Buscar un comercio"
+        />
+        <div className="circle-icon">
+          <SearchIcon />
         </div>
-            )  
-        })
-    )
-    
-    
-    
-    render(){ 
-        const {loading} = this.state;
-    return(
-        <div className='ListCard-container center'>
-                {loading ? this.listtarget():<div className="box"><CircularProgress classes={{svg:'red'}}/></div>}
-        </div>
-    )
-}   
-}
+      </div>
+      <div className="ListCard-container ">
+        {loading ? (
+          filteredData.map((datos) => {
+            const hora = new Date().getHours();
+            const apertura = datos.horario[0].substr(0, 2);
+            const cierre = datos.horario[1].substr(0, 2);
+            return (
+              <div className="cards-list" key={datos.id}>
+                <Link
+                  to={{
+                    pathname: "/detalles",
+                    state: datos,
+                  }}
+                >
+                  <div className="card-img">
+                    {parseInt(hora) >= apertura && parseInt(hora) < cierre ? (
+                      ""
+                    ) : (
+                      <div className="close">
+                        <label>CERRADO</label>
+                      </div>
+                    )}
+                    <img src={datos.url} alt="Imagen" className="card-image" />
+                  </div>
+                  <div className="text-card-footer">
+                    <span>{datos.restaurante}</span>
+                    <span className="active">
+                      {parseInt(hora) >= apertura && parseInt(hora) <= cierre
+                        ? "Abierto"
+                        : "Cerrado"}
+                    </span>
+                  </div>
+                </Link>
+              </div>
+            );
+          })
+        ) : (
+          <div className="box">
+            <CircularProgress classes={{ svg: "red" }} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default ListCards;
---
+++ src/hooks/useSearch.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+const useSearch = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [filtered, setFiltered] = useState([]);
+  const [sourceData, setSourceData] = useState([]);
+
+  const filter = () => {
+    const value = searchValue.toLowerCase().trim().split(" ");
+    console.log(value);
+    console.log(sourceData);
+    const filteredData = sourceData.filter((item, i) => {
+      let res = false;
+      for (const prop in item) {
+        if (value.length === 1) {
+          // una sola palabra
+          const valueTC = String(item[prop]).toLocaleLowerCase();
+          const valueToCompare = valueTC.includes(value[0]);
+          const valueToCompareMod = valueTC
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "")
+            .includes(value[0]);
+
+          if (valueToCompare || valueToCompareMod) res = true;
+        } else {
+          // al menos 2 palabras de busqueda
+          let coincidencias = 0;
+          value.forEach((value, i) => {
+            const valueTC = String(item[prop]).toLocaleLowerCase();
+            const valueToCompare = valueTC.includes(value);
+            const valueToCompareMod = valueTC
+              .normalize("NFD")
+              .replace(/[\u0300-\u036f]/g, "")
+              .includes(value);
+            if (valueToCompare || valueToCompareMod) coincidencias++;
+          });
+
+          if (coincidencias >= value.length) res = true;
+        }
+      }
+      return res;
+    });
+    setFiltered(filteredData);
+  };
+
+  useEffect(() => {
+    filter();
+  }, [searchValue, sourceData]);
+
+  return [filtered, setSearchValue, setSourceData];
+};
+
+export default useSearch;
