468f150c40f0919661d61988c70dd1bd150daad5
Uriel Hernandez <urielch997@gmail.com>
2022-07-01 00:44:44 +0200

    Se cambio funcion a hook no funcional

---
+++ src/hooks/FormLogin/Login.tsx
@@ -0,0 +1,55 @@
+    /**
+     * Verificacion de estado de logeo
+     */
+    export const isLogin = async () => {
+
+    }
+
+    /**
+     * Loggin de usuarios
+     */
+    export const Loggin = async () =>{
+
+    }
+
+    /**
+     * Logut de usuarios
+     */
+    export const Logout = async () =>{
+
+    }
+
+    /**
+     * Registro de usuarios
+     */
+    export const Register = async () =>{
+
+    }
+
+    /**
+     * Refresca la sesion del usuario
+     */
+    export const RefreshToken = async ()=>{
+
+    }
+
+    /**
+     * Guarda token
+     */
+    export const SaveToken = async () =>{
+
+    }
+
+    /**
+     * Iniciar sesion con facebook
+     */
+    export const FacebookLogin = async () =>{
+
+    }
+
+    /**
+     * Iniciar sesion con Google
+     */
+    export const GoogleLogin = async () =>{
+
+    }
